82dda59e6cd9272fba0902a7b37e7f0335d88902
DemiGod <157214254+HEAVENLY-GOD@users.noreply.github.com>
2025-05-25 09:52:32 +0530

    Skeleton tree
    initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_LH8n.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_LH8n-user \
+    lineage_LH8n-userdebug \
+    lineage_LH8n-eng
