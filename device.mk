80de39058c8f9061b49fba0fb66e48eabf14a884
Goku048 <157214254+Goku048@users.noreply.github.com>
2025-06-05 17:46:15 +0530

    LH8n : update : A/b
    * Inherit generic_ramdisk.mk

--- device.mk
+++ device.mk
@@ -10,8 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-# A/B
+# Enable Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
 
 # Dalvik configs
