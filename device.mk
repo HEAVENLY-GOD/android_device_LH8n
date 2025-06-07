9148e6a50a2ddbae25c38408a5e5ac90cca75534
Goku048 <157214254+Goku048@users.noreply.github.com>
2025-06-07 08:33:25 +0530

    LH8n : Switch : to Lineage perfmgr

--- device.mk
+++ device.mk
@@ -320,7 +320,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.mediatek-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub \
     libmtkperf_client_vendor \
     libmtkperf_client
